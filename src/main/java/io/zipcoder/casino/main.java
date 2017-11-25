c63f9b07b485f31c6c1d3752b0f3ba638a4c7e8b
elliott <elliott@zipcoders-MacBook-Pro.local>
2017-11-25 18:26:53 -0500

    updatedCasinoAndMain

--- src/main/java/io/zipcoder/casino/main.java
+++ src/main/java/io/zipcoder/casino/main.java
@@ -3,16 +3,9 @@ package io.zipcoder.casino;
 public class main {
 
 
+
     public static void main(String[] args) {
-        Player player1=new Player("koz", 1000, false);
-
-//      BlackJackGambler player=new BlackJackGambler(player1,10000);
-//      BlackJack blackJack=new BlackJack();
-//      blackJack.play(player);
-
-        GoFishPlayer player=new GoFishPlayer(player1);
-        GoFish goFish=new GoFish();
-        goFish.play(player);
-
+        Casino casino=new Casino();
+        casino.start();
     }
 }
